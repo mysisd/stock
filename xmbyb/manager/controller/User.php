0ab540301bf494fb098225e56644398c8295a51a
陈育智 <17429717762qq.com>
2018-02-26 17:34:38 +0800

    修改

--- xmbyb/manager/controller/User.php
+++ xmbyb/manager/controller/User.php
@@ -16,8 +16,40 @@ class User extends Controller{
     public function Create(){
         echo $this->fetch();
     }
+    public function Creates(){
+       $DeptID=input('DeptID');
+       $StationID=input('StationID');
+       $rolediv=input('rolediv');
+       $User_Name=input('User_Name');
+       $User_Pwd=input('User_Pwd');
+       $RealName=input('RealName');
+       $Email=input('Email');
+       $UserCer=input('UserCer');
+       $UserPhone=input('UserPhone');
+       $StopTrade=input('StopTrade');
+       $EntrustNum=input('EntrustNum');
+       $data['DeptID']=$DeptID;
+       $data['StationID']=$StationID;
+       $data['rolediv']=$rolediv;
+       $data['User_Name']=$User_Name;
+       $data['User_Pwd']=$User_Pwd;
+       $data['RealName']=$RealName;
+       $data['Email']=$Email;
+       $data['UserCer']=$UserCer;
+       $data['UserPhone']=$UserPhone;
+       $data['StopTrade']=$StopTrade;
+       $data['EntrustNum']=$EntrustNum;
+       $data['del']=0;
+       Db('user_management')->strict(false)->insert($data);
+
+    }
     public function CheckUserLimit(){
 
     }
+    public function UserInfoCheck(){
+
+    }
+    public function GetStaionByDept(){}
+
 
 }
